4ad48b14c717ede291be49a14fd82d2054bfe2f7
Huaizhi Ge <gehuaizhi@outlook.com>
2022-08-23 15:20:39 -0400

    detail view

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -10,12 +10,17 @@ import { ProductCategory } from '../common/product-category';
 })
 export class ProductService {
   
-
-
   private baseUrl = 'http://localhost:8080/api/products';
 
   private categoryUrl = 'http://localhost:8080/api/product-category';
   constructor(private httpClient: HttpClient) { }
+
+  getProduct(theProductId: number) :Observable<Product>{
+
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+
+    return this.httpClient.get<Product>(productUrl);
+  }
 
   getProductList(theCategoryId: number) :Observable<Product[]> {
 
